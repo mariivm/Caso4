e04da8bb2015dbdc6f777f2e94f33904865b6ba2
Hytan Jara <80560996+HytanJara@users.noreply.github.com>
2023-05-26 15:09:26 -0600

    Stored procedure Dirty Read y solucion

---
+++ Problemas de concurrencia/SP_DR.sql
@@ -0,0 +1,50 @@
+--Preliminar #1 Caso #4
+-- MARIANA VIQUEZ MONGE
+-- HYTAN JARA MATAMOROS
+-- SP CON PROBLEMAS
+
+Drop procedure if exists lecturaRecipientes
+go
+CREATE PROCEDURE lecturaRecipientes
+	@recipId INT
+AS
+BEGIN
+    SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+    BEGIN TRANSACTION;
+
+    -- Realizar una lectura de datos sin confirmar
+    SELECT * FROM recipientes WHERE recipienteId=@recipId;
+
+    -- Hacer commit de la transacción
+    COMMIT;
+END
+go
+
+exec lecturaRecipientes @recipId=10
+go
+
+--SOLUCION
+--En este caso la solución es sencillamente cambiar el ISOLATION LEVEL a commited
+--de esta forma las columnas que se estan modificando en el stored procedure se bloquearan
+--evitando de esta forma acceso a los datos en caso de que ocurra un error
+
+Drop procedure if exists lecturaRecipientesSol
+go
+CREATE PROCEDURE lecturaRecipientesSol
+	@recipId INT
+AS
+BEGIN
+    SET TRANSACTION ISOLATION LEVEL READ COMMITTED; --Cambiar a Commited
+    BEGIN TRANSACTION;
+
+    -- Realizar una lectura de datos sin confirmar
+    SELECT * FROM recipientes WHERE recipienteId=@recipId;
+
+    -- Hacer commit de la transacción
+    COMMIT;
+END
+go
+
+exec lecturaRecipientesSol @recipId=10
+go
+
